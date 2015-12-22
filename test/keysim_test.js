860995377f4bf894b1fbd3942969d474b91174f9
Shahar Or <mightyiampresence@gmail.com>
2015-12-22 18:32:39 +0200

    Remove the test:node:dist script (also in the test)

--- test/keysim_test.js
+++ test/keysim_test.js
@@ -2,13 +2,7 @@ import "es5-shim";
 
 import assert from 'assert';
 
-// whether we test the source or the dist
-import * as src from '..';
-import * as dist from '../dist/keysim';
-const keysim = process.env.DIST ? dist : src;
-const Keyboard = keysim.Keyboard;
-const Keystroke = keysim.Keystroke;
-const KeyEvents = keysim.KeyEvents;
+import {Keyboard, Keystroke, KeyEvents} from '..';
 
 import isInNode from 'detect-node';
 
