309edfdb806a81df15d73cb7c515ee264b1bddb0
Brian Donovan <me@brian-donovan.com>
2016-07-13 13:02:19 -0700

    refactor: include dom-element-is-natively-editable in the bundle
    This will make it possible for people to simply use keysim.js as-is.

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,14 @@
 import babel from 'rollup-plugin-babel';
 import babelrc from 'babelrc-rollup';
+import nodeResolve from 'rollup-plugin-node-resolve';
 
 var pkg = require('./package.json');
 
 export default {
   entry: 'src/keysim.js',
   plugins: [
-    babel(babelrc())
+    babel(babelrc()),
+    nodeResolve({ jsnext: true })
   ],
   targets: [
     {
